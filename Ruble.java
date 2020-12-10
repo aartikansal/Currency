b8adebef8adcedfe818b485ad9c3bfb3b79cdf7b
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-09 21:25:31 -0500

    Add files via upload

---
+++ Ruble.java
@@ -0,0 +1,12 @@
+
+public class Ruble extends Money {
+
+    Ruble(double amount) {
+        super(amount);
+    }
+
+    @Override
+    public String getCurrencyName() {
+        return "RUB";
+    }
+}
