545e4b465c922d04757e2b60369a7af9a0fa1f16
aartikansal <60615341+aartikansal@users.noreply.github.com>
2020-12-09 21:20:47 -0500

    Add files via upload

---
+++ SolveCurrency.java
@@ -0,0 +1,44 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Currencies
+
+*/
+
+public class SolveCurrency {
+
+    public static void main(String[] args) {
+        Person Aarti = new Person("Aarti");
+        for (Money money : Aarti.getAllMoney()) {
+            System.out.println(Aarti.name + " has a stash of " + money.getAmount() + " " + money.getCurrencyName());
+        }
+    }
+
+    public static class Person {
+        public String name;
+
+        public Person(String name) {
+            this.name = name;
+            this.allMoney = new ArrayList<Money>();
+            allMoney.add(new Euro(1000));
+            allMoney.add(new USD(100));
+            allMoney.add(new Ruble(6000));
+        }
+
+        private List<Money> allMoney;
+
+        public List<Money> getAllMoney() {
+            return allMoney;
+        }
+    }
+}
+
+
+
+
+
+
+
+
